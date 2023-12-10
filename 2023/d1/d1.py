23da5a8a6bce7a083dd01fbd0b23070a6204eb04
Herminio Vazquez <canimus@gmail.com>
2023-12-10 10:54:10 +0000

    Working day 1

--- 2023/d1/d1.py
+++ 2023/d1/d1.py
@@ -5,13 +5,12 @@ from operator import itemgetter as it
 from operator import add
 from operator import methodcaller as mc
 from functools import reduce, partial
-from toolz import compose, remove, juxt
+from toolz import compose, remove, juxt, first, last
 
 raw = Path("input.txt").read_text().split("\n")
 
 _c = compose(int, partial(reduce, add), it(0,-1), list, partial(remove, str.isalpha))
-result = sum(map(_c, raw))
-print(result)
+print(sum(map(_c, raw)))
 
 digits = {
     "one" : 1,
@@ -26,19 +25,29 @@ digits = {
 }
 
 words = list(digits.keys())
+numbers = list(map(str, digits.values()))
+tokens = words + numbers
 
-def convert(word):
-    indices = list(juxt(*[mc("find", o) for o in words])(word))
+def find_digit(word, side : str = "left"):
+    if side == "left":
+        fn = "find"
+        take = first
+    else:
+        fn = "rfind"
+        take = last
+    indices = list(juxt(*[mc(fn, o) for o in tokens])(word))
     A = np.array(indices)
-    matches = np.sort(A[np.where(A >= 0)])
-    for i in matches:
-        position = indices.index(i)
-        d = words[position]
-        if d in word:
-            word = word.replace(d, str(digits[d]),1)
+    match = take(np.sort(A[np.where(A >= 0)]))
+    position = indices.index(match)
+    if position > 8:
+        value = numbers[position - 9]
+    else:
+        value = digits[words[position]]
 
-    return word
+    return str(value)
 
-#raw = Path("test2.txt").read_text().split("\n")
-_w = compose(_c, convert)
+_l = lambda x: find_digit(x, "left")
+_r = lambda x: find_digit(x, "right")
+
+_w = compose(int, partial(reduce, add), juxt(_l, _r))
 print(sum(map(_w, raw)))
