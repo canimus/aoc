e0864407c707627e7bd7faa73480753de4ec3116
Herminio Vazquez <hvazquez-cano@copado.com>
2024-11-30 17:06:14 +0100

    Added 2024

--- n.sh
+++ n.sh
@@ -1,5 +1,5 @@
-mkdir -p 2023/d$1
-touch 2023/d$1/input.txt
-touch 2023/d$1/test.txt
-touch "2023/d$1/d$1.py"
-cp n.json "2023/d$1/x$1.ipynb"
+mkdir -p 2024/d$1
+touch 2024/d$1/input.txt
+touch 2024/d$1/test.txt
+touch "2024/d$1/d$1.py"
+cp n.json "2024/d$1/x$1.ipynb"
